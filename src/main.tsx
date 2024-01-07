193a73a56337de072cfcbb795f87605751628cbe
Shin-sibainu <shincode0712@gmail.com>
2024-01-07 21:23:49 +0900

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+//https://zenn.dev/kazukix/articles/react-setup-2024
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
